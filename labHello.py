cabf5763c4b39cb157b390882205bc556aa6012d
AlexanderKrupenin <108882703+AlexanderKrupenin@users.noreply.github.com>
2023-02-17 21:28:37 +0300

    Add files via upload

---
+++ labHello.py
@@ -0,0 +1,26 @@
+# Я молодец!\
+"""
+т
+е
+с
+т
+"""
+
+print  ("Hello World"*2)
+name = "Masha"
+print (name + " Molodes!")
+print (name , " Molodes!")
+print (f"Vay! Molodes! {name}!")
+
+print(1,2,3,4)
+print(1,2,3,4, sep=", ")
+print(1,2,3,4, sep="\n")
+print(1,2,3,4, end="____")
+print("end")
+
+def AAA():
+    a = 1
+    b = 2
+    print(a + b)
+
+AAA()
